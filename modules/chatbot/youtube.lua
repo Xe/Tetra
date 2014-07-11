6a6903af6ac4a58c634eee93e9081f2737a446c9
Sam Dodrill <xena@yolo-swag.com>
2014-07-11 06:59:25 -0700

    Add basic youtube script

---
+++ modules/chatbot/youtube.lua
@@ -0,0 +1,28 @@
+-- Based on:
+-- https://github.com/TheLinx/Juiz/blob/master/modules/ytvlookup.lua
+
+local json = require "json"
+local http = require "socket.http"
+
+function ytlookup(id)
+  local c = http.request("http://gdata.youtube.com/feeds/api/videos/"..id.."?alt=json&fields=author,title")
+  local res = json.decode(c)
+  local author, title = res.entry.author[1].name["$t"], res.entry.title["$t"]
+
+  return "^ Youtube - " .. title .. " Posted by: " .. author
+end
+
+function youtube_scrape(line)
+  local source, destination, message = parseLine(line)
+
+  if destination.Target():sub(1,1) == "#" then
+    if message:find("youtube%.com/watch") then
+      client.Privmsg(destination, ytlookup(message:match("v=(...........)")))
+    elseif message:find("youtu%.be/") then
+      client.Privmsg(destination, ytlookup(message:match("%.be/(...........)")))
+    else return end
+  end
+end
+
+tetra.script.AddLuaProtohook("PRIVMSG", "youtube_scrape")
+

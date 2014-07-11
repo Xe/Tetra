6ae02dff526027b879e93a054a2e67b080dce0c5
Sam Dodrill <xena@yolo-swag.com>
2014-07-11 08:42:29 -0700

    Add command to list scripts

--- modules/tetra/main.lua
+++ modules/tetra/main.lua
@@ -32,6 +32,13 @@ commands = {
 
     return "Script " .. name .. " unloaded"
   end,
+  SCRIPTS = elevated() .. function(source, message)
+    for name, script in pairs(tetra.bot.Scripts) do
+      client.Notice(source, script.Client.Nick .. ": " .. name .. " (" .. script.Uuid:sub(1,8) .. ")" .. " handlers: " .. #script.Handlers)
+    end
+
+    return "End of scripts list"
+  end,
 }
 
 function parsecommands(source, message)

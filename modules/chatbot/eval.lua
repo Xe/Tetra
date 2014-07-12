534ac49c295b6e869fc4d494ab3776371be1ce8d
Sam Dodrill <xena@yolo-swag.com>
2014-07-12 16:58:34 -0700

    Add chatbot/eval script for testing

---
+++ modules/chatbot/eval.lua
@@ -0,0 +1,33 @@
+ld = nil
+
+function eval_channel(line)
+  local source, destination, message = parseLine(line)
+
+  if not is_common_channel(destination) then
+    return
+  end
+
+  if not source.IsOper() then
+    return
+  end
+
+  ld = destination
+
+  if message:sub(1,1) == "=" then
+    tetra.log.Printf(message)
+    toeval = message:sub(3)
+
+    local func, err = loadstring(toeval)
+
+    if err ~= nil then
+      client.Privmsg(destination, string.format("error: %s",(err)))
+      return
+    end
+
+    local res, err = func()
+
+    tetra.log.Printf("%#v: %#v", res, err)
+  end
+end
+
+tetra.script.AddLuaProtohook("PRIVMSG", "eval_channel")

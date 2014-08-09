b35074a201f77dc46508ba3dbfd22dd6adeaff33
Sam Dodrill <xena@yolo-swag.com>
2014-08-09 08:25:15 -0700

    Make main more robust, 12 factor configuration

--- main.go
+++ main.go
@@ -2,11 +2,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/Xe/Tetra/bot"
 )
 
 func main() {
-	bot := tetra.NewTetra("etc/config.json")
+	confloc := os.Getenv("TETRA_CONFIG_PATH")
+
+	if confloc == "" { // No user set config location
+		var file *os.File
+		var err error
+		if file, err = os.Open("etc/config.json") ; err != nil {
+			fmt.Fprintln(os.Stderr, "Please either set TETRA_CONFIG_PATH to the location of the configuration file or add your config at etc/config.json")
+		} else {
+			confloc = "etc/config.json"
+			file.Close()
+		}
+	}
+
+	bot := tetra.NewTetra(confloc)
 
 	bot.Connect(bot.Config.Uplink.Host, bot.Config.Uplink.Port)
 	defer bot.Conn.Conn.Close()

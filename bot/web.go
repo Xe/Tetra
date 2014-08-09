214db85fd70bb5dd86f8573270a3dced837fc698
Sam Dodrill <xena@yolo-swag.com>
2014-08-09 10:08:03 -0700

    Add yaml api

--- bot/web.go
+++ bot/web.go
@@ -6,11 +6,79 @@ import (
 	"os"
 
 	"github.com/codegangsta/negroni"
+	"gopkg.in/yaml.v1"
 )
+
+type client struct {
+	Nick    string
+	User    string
+	Host    string
+	Account string
+	Joins   []chanuser
+}
+
+type chanuser struct {
+	Client  string
+	Channel string
+	Prefix  int
+}
+
+type channel struct {
+	Name    string
+	Ts      int64
+	Modes   int
+	Clients []chanuser
+}
+
+func convertChannel(in *Channel) (out channel) {
+	out = channel{
+		Name:  in.Name,
+		Ts:    in.Ts,
+		Modes: in.Modes,
+	}
+
+	for _, chanclient := range in.Clients {
+		out.Clients = append(out.Clients, chanuser{
+			Client:  chanclient.Client.Nick,
+			Channel: chanclient.Channel.Name,
+			Prefix:  chanclient.Prefix,
+		})
+	}
+
+	return
+}
 
 func (t *Tetra) WebApp() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", index)
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(res, "error: No method chosen.")
+	})
+	mux.HandleFunc("/config.yaml", func(res http.ResponseWriter, req *http.Request) {
+		out, err := yaml.Marshal(t.Config)
+		if err != nil {
+			res.WriteHeader(500)
+			fmt.Fprintf(res, `error: Bad yaml`)
+			return
+		}
+
+		fmt.Fprintf(res, "%s", out)
+	})
+	mux.HandleFunc("/channels.yaml", func(res http.ResponseWriter, req *http.Request) {
+		var channels []channel
+
+		for _, in := range t.Channels {
+			channels = append(channels, convertChannel(in))
+		}
+
+		out, err := yaml.Marshal(channels)
+		if err != nil {
+			res.WriteHeader(500)
+			fmt.Fprintf(res, `error: Bad yaml`)
+			return
+		}
+
+		fmt.Fprintf(res, "%s", out)
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -31,8 +99,4 @@ func (t *Tetra) WebApp() {
 			t.Log.Fatal(err)
 		}
 	}()
-}
-
-func index(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "{\"error\": \"No method chosen.\"}\n")
 }

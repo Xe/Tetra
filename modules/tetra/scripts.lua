234069062fe36f632e8612712a21690aa6b9fb67
Sam Dodrill <xena@yolo-swag.com>
2014-08-10 08:24:08 -0700

    tetra/scripts: show number of loaded commands

--- modules/tetra/scripts.lua
+++ modules/tetra/scripts.lua
@@ -1,6 +1,6 @@
 SCRIPTS = elevated() .. function(source, message)
   for name, script in pairs(tetra.bot.Scripts) do
-    client.Notice(source, script.Client.Nick .. ": " .. name .. " (" .. script.Uuid:sub(1,8) .. ")" .. " handlers: " .. #script.Handlers)
+    client.Notice(source, script.Client.Nick .. ": " .. name .. " (" .. script.Uuid:sub(1,8) .. ")" .. " handlers: " .. #script.Handlers .. " commands: " .. #script.Commands)
   end
 
   return "End of scripts list"

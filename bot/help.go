0bcf508039df0a16556d11d38923b0c78636cf5b
Sam Dodrill <xena@yolo-swag.com>
2014-08-11 21:16:26 -0700

    Show core templates for core commands

--- bot/help.go
+++ bot/help.go
@@ -11,6 +11,10 @@ import (
 func helpHas(service string, command string) bool {
 	command = strings.Join(strings.Split(command, " "), "_")
 
+	if command == "help" || command == "_index" {
+		service = "tetra"
+	}
+
 	_, err := os.Stat("help/" + service + "/" + command)
 
 	return err == nil
@@ -18,6 +22,10 @@ func helpHas(service string, command string) bool {
 
 func (c *Client) showHelp(target *Client, service, command string) {
 	buffer := bytes.NewBufferString("")
+
+	if command == "help" || command == "_index" {
+		service = "tetra"
+	}
 
 	text := template.New(service + "/" + command)
 	helptext, _ := ioutil.ReadFile("help/" + service + "/" + command)

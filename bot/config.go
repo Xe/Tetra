d60f6ed6b552a4517cf1fe05219b2b51f54f5efe
Sam Dodrill <xena@yolo-swag.com>
2014-08-12 18:44:11 -0700

    bot/config: add Config.Modules field for modules to specify configuration parameters

--- bot/config.go
+++ bot/config.go
@@ -56,6 +56,7 @@ type Config struct {
 	Stats    *StatsConfig
 	ApiKeys  map[string]string
 	General  *GeneralConfig
+	Modules  map[string]interface{}
 }
 
 // NewConfig returns a new Config instance seeded by the file at fname.

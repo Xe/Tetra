4271e6309427f9ee66d6a4cd2a3ba623e20df7cd
Sam Dodrill <xena@yolo-swag.com>
2014-08-13 21:17:32 -0700

    chatbot/youtube: Update

--- modules/chatbot/youtube.lua
+++ modules/chatbot/youtube.lua
@@ -5,15 +5,14 @@ local json = require "json"
 local http = require "socket.http"
 
 function ytlookup(id)
-  local c = http.request("http://gdata.youtube.com/feeds/api/videos/"..id.."?alt=json&fields=author,title")
-  local res = json.decode(c)
+  local res = getjson("http://gdata.youtube.com/feeds/api/videos/"..id.."?alt=json&fields=author,title")
   local author, title = res.entry.author[1].name["$t"], res.entry.title["$t"]
 
   return "^ Youtube - " .. title .. " Posted by: " .. author
 end
 
 Hook("CHATBOT-CHANMSG", function(source, destination, message)
-  message = strings.join(message, " ")
+  message = table.concat(luar.slice2table(message), " ")
 
   if message:find("youtube%.com/watch") then
     client.Privmsg(destination, ytlookup(message:match("v=(...........)")))

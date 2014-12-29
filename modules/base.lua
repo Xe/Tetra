bf725a0250b17e225960db56ad2f14f405202809
Sam Dodrill <xena@yolo-swag.com>
2014-12-28 16:19:16 -0800

    modules/base: add url_encode function to base load

--- modules/base.lua
+++ modules/base.lua
@@ -152,3 +152,14 @@ end
 -- Golua sucks
 pcall = unsafe_pcall
 xpcall = unsafe_xpcall
+
+function url_encode(str)
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w %-%_%.%~])",
+    function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, " ", "+")
+  end
+  return str
+end
+
--- modules/chatbot/bitly.lua
+++ modules/chatbot/bitly.lua
@@ -1,16 +1,6 @@
 -- Shortens long URL's with bit.ly
 -- Needs a bit.ly api key
 
-function url_encode(str)
-  if (str) then
-    str = string.gsub (str, "\n", "\r\n")
-    str = string.gsub (str, "([^%w %-%_%.%~])",
-    function (c) return string.format ("%%%02X", string.byte(c)) end)
-    str = string.gsub (str, " ", "+")
-  end
-  return str
-end
-
 function scrapeurl(str)
   return str:match("https?://[%w-_%.%?%.:/%+=&]+")
 end
